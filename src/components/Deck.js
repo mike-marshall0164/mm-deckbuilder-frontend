936f5b1cb724434beaa888251b1fa850ebded3ef
Mike Marshall <onewingedangel0164@gmail.com>
2018-11-27 20:10:16 -0800

    oops

---
+++ src/components/Deck.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import Card from './Card';
+var config = require('../config');
+
+class Deck extends Component {
+    render() {
+
+        return (
+        <div className="container bg-dark text-light border">
+            <p>Deck</p>
+            <div className="row">
+            <ul>
+                
+            </ul>
+            </div>
+        </div>
+        );
+    }
+
+}
+
+export default Deck;
---
+++ src/components/Nav.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+var config = require('../config');
+
+class Nav extends Component {
+    render() {
+
+        if (this.props.hasErrored) {
+            return <p>Sorry! There was an error loading the items</p>;
+        }
+
+        if (this.props.isLoading) {
+            return <p>Loading…</p>;
+        }
+
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="./">
+                    <img className="logo-img-top float-left p-1" src={config.img_urls.logo} alt="logo" />
+                </a>
+                
+                <h2>Minion Masters Deckbuilder</h2>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    </ul>
+                </div>
+            </nav>
+        );
+
+    }
+}
+
+export default Nav;
